f139830f1f8ac8c1f8bb437a4ac0a115a1b24cb0
dvnerite <57559064+dvnerite@users.noreply.github.com>
2020-01-25 16:55:29 -0800

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
 // write your code here
+     $.getJSON("data.json", function(data) {
+        var top_spot_data = ' '
+        $.each(data, function(item, index) {
+        //console.log(data);
+            top_spot_data += '<tr>';
+            top_spot_data += '<td>'+index.name+'</td>';
+            top_spot_data += '<td>'+index.description+'</td>';
+            top_spot_data +='<td><a href="https://www.google.com/maps?q=' + index.location + ">" + "Open In Google Maps" + '</a></td>';
+                            //'<td><a href="https://www.google.com/maps?q=' + index.location + '">' + "open in google" + '</a></td>';
+            top_spot_data += '<tr>';
 
-});
+        
+        console.log(index.name);
+        console.log(index.description);
+        console.log(index.link);
+
+    //$("<td>").append(index.name + " ");
+    //$("").append(index.description + " ");
+    //$("").append(index.link + " ");
+
+            }); 
+    $("#top_spot_table").append(top_spot_data);
+        })
+    });
